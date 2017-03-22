51de2e5547e98dcae21fb2ddcd3e1978fab7315e
Christian Hartman <cjhartmanx@gmail.com>
2017-03-22 19:16:32 -0400

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//pulling data from database
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+//request, response, next
+router.get('/posts', function(req, res, next){
+    Post.find(function(err, posts){
+        if (err){ next(err); }
+        res.json(posts);
+    });
+});
+
+router.post('/posts', function(req, res, next){
+    var post = new Post(req.body);
+    
+    post.save(function(err, post){
+        if(err){return next(err); }
+        res.json(post);
+    })
+});
+
+module.exports = router;
