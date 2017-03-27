cbc008ff954e3de85df62b7d646ce898f7e75508
Christian Hartman <cjhartmanx@gmail.com>
2017-03-27 17:08:24 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ var Comment = mongoose.model('Comment');
 //request, response, next
 router.get('/posts', function(req, res, next){
     Post.find(function(err, posts){
-        if (err){ next(err); }
+        if (err){return next(err); }
         res.json(posts);
     });
 });
@@ -26,6 +26,67 @@ router.post('/posts', function(req, res, next){
         if(err){return next(err); }
         res.json(post);
     })
+});
+
+router.param('post', function(req, res, next, id){
+    var query = Post.findById(id);
+    
+    query.exec(function (err, post){
+        if (err){return next(err); }
+        if(!post) {return next(new Error("can\'t find a post"));}
+        req.post = post;
+        return next();
+    });
+});
+
+router.param('comment', function(req, res, next, id){
+    var query = Comment.findById(id);
+    
+    query.exec(function (err, comment){
+        if (err){return next(err); }
+        if(!comment) {return next(new Error("can\'t find a comment"));}
+        req.comment = comment;
+        return next();
+    });
+});
+
+router.get('/posts/:post', function(req, res){
+    req.post.populate('comments', function(err, post){ 
+        if (err) { return next(err); }
+        
+        res.json(post);
+    });
+});
+
+router.put('/posts/:post/upvote', function(req, res,next){
+    req.post.upvote(function(err,post){
+        if(err){return next(err); }
+        res.json(post);
+    });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res,next){
+    req.comment.upvote(function(err, comment){
+        if(err){return next(err); }
+        res.json(comment);
+    });
+});
+
+
+router.post('/posts/:post/comments', function(req,res,next){
+    var comment = new Comment(req.body);
+    comment.post = req.post;
+    
+    comment.save(function(err, comment){
+        if(err){return next(err);}
+        
+        req.post.comments.push(comment);
+        req.post.save(function(err,post){
+            if(err){return next(err);}
+
+        res.json(comment);
+        });
+    });
 });
 
 module.exports = router;
